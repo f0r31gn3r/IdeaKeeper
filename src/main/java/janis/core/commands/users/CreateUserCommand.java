d0f63386a0a84c8e844f55b729a54d0b5926304f
Janis Karklins <jjankar@gmail.com>
2015-11-04 18:49:36 +0200

    users commands

---
+++ src/main/java/janis/core/commands/users/CreateUserCommand.java
@@ -0,0 +1,67 @@
+package janis.core.commands.users;
+
+import lv.javaguru.java3.core.commands.DomainCommand;
+
+/**
+ * Created by Anna on 27.10.2015.
+ */
+public class CreateUserCommand implements DomainCommand<CreateUserResult> {
+
+    private String login;
+    private String password;
+    private String name;
+    private String surname;
+    private String email;
+    private String accessLevel;
+
+
+    public CreateUserCommand(String login,
+                             String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public CreateUserCommand(String login,
+                             String password,
+                             String name,
+                             String surname) {
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public CreateUserCommand(String login,
+                             String password,
+                             String name,
+                             String surname,
+                             String email,
+                             String accessLevel) {
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.accessLevel = accessLevel;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() { return email; }
+
+    public String getAccessLevel() { return accessLevel; }
+}
